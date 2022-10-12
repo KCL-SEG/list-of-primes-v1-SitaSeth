647f4d2c259d2715e0378694c679e0e975b58885
Sita Sethurajan <k21034097@kcl.ac.uk>
2022-10-12 09:15:27 +0100

    completed primes function. All tests passed

--- primes.py
+++ primes.py
@@ -3,4 +3,23 @@
 
 def primes(number_of_primes):
     list = []
+    x=2 
+    while(len(list)<number_of_primes):
+        if(isPrime(x)):
+            list.append(x)
+    
+        x+=1   
     return list
+
+def isPrime(n):
+    factor_count=0;
+    x=1; 
+    while(x<=n):
+        if(n%x==0):
+            factor_count+=1 
+        x+=1
+    if(factor_count>2):
+        return False
+    return True
+
+
